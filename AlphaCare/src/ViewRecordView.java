4e67dda543de0862faea670376f17edde1203d91
Angie DeMarco <angie.demarco98@gmail.com>
2019-09-21 02:00:51 -0400

    updating ViewRecordView to reflect attributes

--- AlphaCare/src/ViewRecordView.java
+++ AlphaCare/src/ViewRecordView.java
@@ -5,56 +5,28 @@ public class ViewRecordView extends JFrame {
     private JTextField medicalRecordField;
     private MedicalRecord medicalRecord;
 
-    private RecordModel model;
+    //private RecordModel model;
 
     ViewRecordView(RecordModel model) {
         this.model = model;
 
-        currentWordField = new JTextField(20);
-        getWordButton = new JButton("Get new word");
-
-        enterWordField = new JTextField(20);
-        putWordButton = new JButton("Enter new word");
+        this.medicalRecordField = new JTextField(20);
+        this.editRecordButton = new JButton("Edit Record");
 
         JPanel content = new JPanel();
         content.setLayout(null);
-        content.add(currentWordField);
-        content.add(getWordButton);
-        content.add(enterWordField);
-        content.add(putWordButton);
+        content.add(this.medicalRecordField);
+        content.add(this.editRecordButton);
 
         //setting positions and dimensions of everything
-        currentWordField.setBounds(5, 5, 300, 50);
-        getWordButton.setBounds(5, 60, 200, 50);
-        enterWordField.setBounds(350, 5, 300, 50);
-        putWordButton.setBounds(350, 60, 200, 50);
+        medicalRecordField.setBounds(5, 5, 300, 50);
+        editRecordButton.setBounds(5, 60, 200, 50);
 
         this.setContentPane(content);
-        this.pack();
-        this.setTitle("Fancy Random Word MVC Example");
+        this.setTitle("Record");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.setSize(800, 600);
-    }
-
-    @Override
-    public String getGivenWord() {
-        return enterWordField.getText();
-    }
-
-    @Override
-    public void addGetWordButtonListener(ActionListener al) {
-        getWordButton.addActionListener(al);
-    }
-
-    @Override
-    public void setCurrentWord(String newWord) {
-        currentWordField.setText(newWord);
-    }
-
-    @Override
-    public void addPutWordButtonListener(ActionListener al) {
-        putWordButton.addActionListener(al);
     }
 
 }
