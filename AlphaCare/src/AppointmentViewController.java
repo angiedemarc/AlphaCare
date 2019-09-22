2c7c7b15f50d0a678a80bfb1305d86fd4c1fa575
Calvin <cvh5567@psu.edu>
2019-09-22 19:12:08 -0400

    Joey's controllers

---
+++ AlphaCare/src/AppointmentViewController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author calvinho
+ */
+public class AppointmentViewController {
+    
+      // Attributes in order to develop controller for the LoginPageViewController.
+    private AppointmentView appointmentView;
+    private RecordModel model;
+    
+    // Constructor for LoginPageViewController.
+    public AppointmentViewController(AppointmentView appointmentView, RecordModel model) {
+        this.appointmentView = appointmentView;
+        this.model = model;
+        // Method in order to add createRecordView to the controller class. 
+        addAppointmentView();
+    }
+    // Added view to the controller class. 
+    public void addAppointmentView() {
+        this.appointmentView = new AppointmentView();
+    }
+    
+}
+
+    
+}
---
+++ AlphaCare/src/LoginPageViewController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author calvinho
+ */
+public class LoginPageViewController {
+    
+     // Attributes in order to develop controller for the LoginPageViewController.
+    private LoginPageView loginPageView;
+    private RecordModel model;
+    
+    // Constructor for LoginPageViewController.
+    public LoginPageViewController(LoginPageView loginPageView, RecordModel model) {
+        this.loginPageView = loginPageView;
+        this.model = model;
+        // Method in order to add createRecordView to the controller class. 
+        addLoginPageView();
+    }
+    // Added view to the controller class. 
+    public void addLoginPageView() {
+        this.loginPageView = new LoginPageView();
+    }
+    
+}
