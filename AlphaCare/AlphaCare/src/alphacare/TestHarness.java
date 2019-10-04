f96684e2c26b83b8eb9a045f3285cb4e54bd34cc
Owner <Owner@DESKTOP-UC5HSVS>
2019-10-04 11:01:34 -0400

    Created TestHarness class with main method.

--- AlphaCare/AlphaCare/src/alphacare/TestHarness.java
+++ AlphaCare/AlphaCare/src/alphacare/TestHarness.java
@@ -16,6 +16,18 @@ public class TestHarness {
      */
     public static void main(String[] args) {
        
+        /*
+         Create a module called testharness as an application to exercise (call) your APIs. The testharness module 
+        should simulate the flow of control you intend for your application. In general, this should consist of 
+        a) controller components passing control between themselves as envisioned in your design, and 
+        b) controller components calling the APIs of their view and model components, again as envisioned in your design.
+        The testharness module should include a main method and be runnable. When it is run it should execute a series of 
+        tests with appropriate output statements communicating whether the tests have passed or failed. The test harness module 
+        will need to instantiate at least one controller class to begin the test, which in turn instantiate other controller, view, 
+        and model classes and exercise their APIs. You may want to include test output statements in your method stubs to reduce 
+        coupling between the testharness module and your other modules.
+        */
+        
     }
     
 }
