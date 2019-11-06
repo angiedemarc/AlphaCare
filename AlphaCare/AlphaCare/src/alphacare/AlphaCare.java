6b7065c32bd8f4600076b5518e391c283be03776
Cole Mossel <cojomo93@gmail.com>
2019-11-06 18:45:09 -0500

    Delete TestHarness Class

--- AlphaCare/AlphaCare/src/alphacare/AlphaCare.java
+++ AlphaCare/AlphaCare/src/alphacare/AlphaCare.java
@@ -5,6 +5,10 @@
  */
 package alphacare;
 
+import Controller.LoginPageViewController;
+import Model.Record;
+import Model.SingletonRecordList;
+
 /**
  *
  * @author cole
@@ -14,8 +18,21 @@ public class AlphaCare {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String[] args) {
-//        // TODO code application logic here
-//    }
+    public static void main(String[] args) {
+         // Shows the functionality of the 'createRecord' which in turn can view a record
+       // CreateRecordViewController createRecord = new CreateRecordViewController();
+        // This tests to see the application work flow from the Login. It should display all of our view classes from just initially the login process. 
+        LoginPageViewController loginPage = new LoginPageViewController(); // We have not yet added authentication functionality to the application yet!
+        
+        
+        // Here, we will test to see the attributes in our model class. 
+
+        System.out.println(loginPage.getTest().toString());
+        //System.out.println(createRecord.getCreateRecordView().getMedicalRecord());
+        
+        for(Record r: SingletonRecordList.getInstance().getRecordList()){
+            System.out.println("From the singleton class: " + r.getPatientName());
+        }
+    }
     
 }
