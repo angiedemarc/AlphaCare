8037c370021f1d3ecbb6f16f6c04a59c1180901f
Owner <Owner@DESKTOP-UC5HSVS>
2019-11-19 11:37:13 -0500

    AppointmentView buttons work now.

--- AlphaCare/AlphaCare/src/Controller/AppointmentViewController.java
+++ AlphaCare/AlphaCare/src/Controller/AppointmentViewController.java
@@ -5,13 +5,14 @@ import Model.*;
 import View.MainInterfaceView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author calvinho
@@ -19,59 +20,63 @@ import java.awt.event.ActionListener;
 public class AppointmentViewController {
 
     private AppointmentView appointmentView;
+    // private Account test;
 
-    public Account getTest() {
-        return test;
-    }
-    private Account test;
-    
     public AppointmentViewController() {
-        AppointmentView appointmentPanel = new AppointmentView();
+        this.appointmentView = new AppointmentView();
+        createButtonListeners();
     }
-    
+
     public void addAppointmentView() {
         this.appointmentView = new AppointmentView();
     }
 
     public AppointmentView getAppointmentView() {
-        return appointmentView;
+        return this.appointmentView;
     }
-    
-    private void createButtonListeners(){
-        appointmentView.getHomeButton().addActionListener(new ActionListener() {
+
+    private void createButtonListeners() {
+        this.appointmentView.getHomeButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent eventL) {
-                System.out.println("home button");
                 MainInterfaceView mainInterface = new MainInterfaceView();
-                
+
                 appointmentView.setVisible(false);
                 mainInterface.setVisible(true);
             }
         });
-        
-        appointmentView.getSubmitButton().addActionListener(new ActionListener() {
+
+        this.appointmentView.getSubmitButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent eventLis) {
-                System.out.println("Testing: Successful Submission!");
-                
+                JLabel submitMessage = new JLabel();
+                JOptionPane.showMessageDialog(submitMessage, "Your appointment has been submitted!");
+
                 MainInterfaceView mainInterface = new MainInterfaceView();
-                
                 appointmentView.setVisible(false);
                 mainInterface.setVisible(true);
             }
         });
-        
-        appointmentView.getCancelButton().addActionListener(new ActionListener() {
+
+        this.appointmentView.getCancelButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent eventList) {
-                System.out.println("Testing: Successful Cancellation!");
-                
-                MainInterfaceView mainInterface = new MainInterfaceView();
-                
-                appointmentView.setVisible(false);
-                mainInterface.setVisible(true);
+
+                JLabel cancelLabel = new JLabel();
+
+                int response = JOptionPane.showConfirmDialog(cancelLabel, "Are you sure you want to cancel scheduling an appointment?", "Confirm",
+                        JOptionPane.YES_OPTION, JOptionPane.NO_OPTION);
+
+                if (response == JOptionPane.YES_OPTION) {
+
+                    MainInterfaceView mainInterface = new MainInterfaceView();
+
+                    appointmentView.setVisible(false);
+                    mainInterface.setVisible(true);
+                }
+
             }
         });
     }
-    
+
 }
