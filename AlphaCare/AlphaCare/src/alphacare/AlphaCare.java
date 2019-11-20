44cd2d64651e61eb65810c712c34c0fc7f703f7b
Owner <Owner@DESKTOP-UC5HSVS>
2019-11-19 19:37:17 -0500

    Global lists of appointments is located in AppointmentViewController.

--- AlphaCare/AlphaCare/src/alphacare/AlphaCare.java
+++ AlphaCare/AlphaCare/src/alphacare/AlphaCare.java
@@ -16,20 +16,11 @@ public class AlphaCare {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Shows the functionality of the 'createRecord' which in turn can view a record
-                // CreateRecordViewController createRecord = new CreateRecordViewController();
-       
-                // This tests to see the application work flow from the Login. It should display all of our view classes from just initially the login process. 
+
         AccountList gr = new AccountList();
-        
         
         LoginPageViewController loginPage = new LoginPageViewController(gr); // We have not yet added authentication functionality to the application yet!
 
-        // Here, we will test to see the attributes in our model class. 
-//        
-//        for(Record r: SingletonRecordList.getInstance().getRecordList()){
-//            System.out.println("From the singleton class: " + r.getPatientName());
-//        }
     }
 
     //checks if records are outdated and updates the list to no longer include them
